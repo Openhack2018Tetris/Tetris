8b3da1f22131f00fb1c488d8284e6bc75958f400
SonMinWoo <31719811+SonMinWoo@users.noreply.github.com>
2018-06-29 10:35:39 +0900

    회원가입 추가

---
+++ app/src/main/java/company/co/kr/tugether/RegistActivity.java
@@ -0,0 +1,160 @@
+package company.co.kr.tugether;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegistActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etPasswordConfirm;
+    private Button btnStudent;
+    private Button btnCancel;
+    private Button btnTeacher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regist);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etPasswordConfirm = (EditText) findViewById(R.id.etPasswordConfirm);
+        btnStudent = (Button) findViewById(R.id.btnStudent);
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnTeacher = (Button) findViewById(R.id.btnTeacher);
+
+        /* 비밀번호 일치 검사 */
+        etPasswordConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String password = etPassword.getText().toString();
+                String confirm = etPasswordConfirm.getText().toString();
+
+                if( password.equals(confirm) ) {
+                    etPassword.setBackgroundColor(Color.GREEN);
+                    etPasswordConfirm.setBackgroundColor(Color.GREEN);
+                } else {
+                    etPassword.setBackgroundColor(Color.RED);
+                    etPasswordConfirm.setBackgroundColor(Color.RED);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        btnStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 이메일 입력 확인
+                if( etEmail.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistActivity.this, "Email을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 입력 확인
+                if( etPassword.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistActivity.this, "비밀번호를 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 확인 입력 확인
+                if( etPasswordConfirm.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistActivity.this, "비밀번호 확인을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etPasswordConfirm.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 일치 확인
+                if( !etPassword.getText().toString().equals(etPasswordConfirm.getText().toString()) ) {
+                    Toast.makeText(RegistActivity.this, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+                    etPassword.setText("");
+                    etPasswordConfirm.setText("");
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                Intent result = new Intent(getApplicationContext(), MainActivity.class);
+                result.putExtra("email", etEmail.getText().toString());
+
+                // 자신을 호출한 Activity로 데이터를 보낸다.
+                //setResult(RESULT_OK, result);
+                startActivity(result);
+                finish();
+            }
+        });
+
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // 이메일 입력 확인
+                if( etEmail.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistActivity.this, "Email을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etEmail.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 입력 확인
+                if( etPassword.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistActivity.this, "비밀번호를 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 확인 입력 확인
+                if( etPasswordConfirm.getText().toString().length() == 0 ) {
+                    Toast.makeText(RegistActivity.this, "비밀번호 확인을 입력하세요!", Toast.LENGTH_SHORT).show();
+                    etPasswordConfirm.requestFocus();
+                    return;
+                }
+
+                // 비밀번호 일치 확인
+                if( !etPassword.getText().toString().equals(etPasswordConfirm.getText().toString()) ) {
+                    Toast.makeText(RegistActivity.this, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+                    etPassword.setText("");
+                    etPasswordConfirm.setText("");
+                    etPassword.requestFocus();
+                    return;
+                }
+
+                Intent result = new Intent(getApplicationContext(), TeacherMain.class);
+                result.putExtra("email", etEmail.getText().toString());
+
+                // 자신을 호출한 Activity로 데이터를 보낸다.
+                //setResult(RESULT_OK, result);
+                startActivity(result);
+                finish();
+            }
+        });
+
+    }
+}
+
